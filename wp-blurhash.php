2a15b4092534a86efc42fdd6832b517c4ae0d0aa
Kyle Hotchkiss <108172+kylehotchkiss@users.noreply.github.com>
2024-09-07 13:37:16 -0700

    Try following wp plugin best practices

--- wp-blurhash.php
+++ wp-blurhash.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: WP Blurhash
  * Plugin URI: https://kylehotchkiss.com/wp-blurhash
@@ -13,18 +14,22 @@
 defined('ABSPATH') || exit;
 
 // Include the Composer autoload file.
-require_once __DIR__ . '/vendor/autoload.php';
+if (is_readable(__DIR__ . '/vendor/autoload.php')) {
+  require __DIR__ . '/vendor/autoload.php';
+}
 
 // Load Blurhash library
 use kornrunner\Blurhash\Blurhash;
 
 // Check if ACF is activated
-function blurhash_acf_activated() {
+function blurhash_acf_activated()
+{
   return class_exists('ACF');
 }
 
 // Create ACF fields
-function blurhash_acf_activate() {
+function blurhash_acf_activate()
+{
   if (!class_exists('ACF')) {
     return false;
   }
@@ -52,7 +57,8 @@ function blurhash_acf_activate() {
 }
 
 // Resize image to 32x32 so that blurhash can be processed pixel by pixel
-function resize_image_before_blurhash($image_path) {
+function resize_image_before_blurhash($image_path)
+{
   // Load the image
   $image = wp_get_image_editor($image_path);
 
@@ -71,7 +77,8 @@ function resize_image_before_blurhash($image_path) {
 }
 
 // Generate blurhash for an image
-function generate_blurhash($image_path) {
+function generate_blurhash($image_path)
+{
   //error_log("Generating blurhash for $image_path");
   //error_log('Image: ' . file_get_contents($image_path));
   $pixels = [];
@@ -80,15 +87,15 @@ function generate_blurhash($image_path) {
   $height = imagesy($image);
 
   for ($y = 0; $y < $height; ++$y) {
-      $row = [];
+    $row = [];
 
-      for ($x = 0; $x < $width; ++$x) {
-          $index = imagecolorat($image, $x, $y);
-          $colors = imagecolorsforindex($image, $index);
+    for ($x = 0; $x < $width; ++$x) {
+      $index = imagecolorat($image, $x, $y);
+      $colors = imagecolorsforindex($image, $index);
 
-          $row[] = [$colors['red'], $colors['green'], $colors['blue']];
-      }
-      $pixels[] = $row;
+      $row[] = [$colors['red'], $colors['green'], $colors['blue']];
+    }
+    $pixels[] = $row;
   }
 
   $components_x = 4;
@@ -100,7 +107,8 @@ function generate_blurhash($image_path) {
 }
 
 // Save blurhash when a media is uploaded
-function save_blurhash_on_upload($attachment_id) {
+function save_blurhash_on_upload($attachment_id)
+{
   $file_path = get_attached_file($attachment_id);
 
   if ($file_path) {
@@ -127,7 +135,8 @@ function save_blurhash_on_upload($attachment_id) {
 
 
 // WP Cron task to generate 10 blurhashes
-function cron_generate_blurhashes() {
+function cron_generate_blurhashes()
+{
   error_log('Generating blurhashes for 50 uploaded images via wp-cron');
 
   // Get 10 attachments without blurhash
@@ -154,7 +163,8 @@ if (!wp_next_scheduled('cron_generate_blurhashes_hook')) {
 }
 
 // Admin page to display blurhash statistics
-function blurhash_admin_page() {
+function blurhash_admin_page()
+{
   // Get total attachments count
   $total_attachments = wp_count_posts('attachment')->inherit;
 
@@ -175,20 +185,22 @@ function blurhash_admin_page() {
   echo '<button id="js-blurhash-run-cron" class="button button-primary">Process 50 more images <span id="blurhash-loader" style="display: none;"><img src="' . admin_url('images/loading.gif') . '" alt="Loading..."></span></button>';
 }
 
-function blurhash_enqueue_admin_js($hook) {
+function blurhash_enqueue_admin_js($hook)
+{
   // Check if we're on our plugin's admin page
   if ('media_page_blurhash_stats' == $hook) {
-      wp_enqueue_script('blurhash-admin', plugins_url('assets/blurhash-admin.js', __FILE__), array('jquery'), '1.0.0', true);
+    wp_enqueue_script('blurhash-admin', plugins_url('assets/blurhash-admin.js', __FILE__), array('jquery'), '1.0.0', true);
 
-      // Add localized variables to the script
-      wp_localize_script('blurhash-admin', 'blurhash_vars', array(
-          'ajax_url' => admin_url('admin-ajax.php'),
-          'nonce' => wp_create_nonce('blurhash_nonce')
-      ));
+    // Add localized variables to the script
+    wp_localize_script('blurhash-admin', 'blurhash_vars', array(
+      'ajax_url' => admin_url('admin-ajax.php'),
+      'nonce' => wp_create_nonce('blurhash_nonce')
+    ));
   }
 }
 
-function run_blurhash_cron_callback() {
+function run_blurhash_cron_callback()
+{
   check_ajax_referer('blurhash_nonce', 'nonce');
 
   // Run your cron task here
@@ -216,7 +228,8 @@ function run_blurhash_cron_callback() {
   ));
 }
 
-function blurhash_admin_menu() {
+function blurhash_admin_menu()
+{
   add_media_page('Blurhash Statistics', 'Blurhash', 'manage_options', 'blurhash_stats', 'blurhash_admin_page');
 }
 
@@ -227,5 +240,3 @@ add_action('admin_enqueue_scripts', 'blurhash_enqueue_admin_js');
 add_action('wp_ajax_run_blurhash_cron', 'run_blurhash_cron_callback');
 add_action('add_attachment', 'save_blurhash_on_upload');
 add_action('cron_generate_blurhashes_hook', 'cron_generate_blurhashes');
-
-?>
